93ecc474a55b2f3800c9eef72ce893263d80df4e
shubhangi <shubhangishetake15@gmail.com>
2023-10-08 13:19:33 +0530

    Home component with basic routing

---
+++ code/Hello/home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("This is home page !!")
+
+def about(request):
+    return HttpResponse("This is about page !!")
+
+def contact(request):
+    return HttpResponse("This is contact page !!")
